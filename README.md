a8ad8a5017eca8906e171915e9f83e44d6f5b732
Stephan Schmitz <stephan.schmitz@sunzinet.com>
2019-02-12 10:01:30 +0100

    More API calls wrapped

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ const aafApi = require('node-aaf-api');
 If you want to send your custom GraphQL queries, use:
 
 ```js
+let customGraphqlQuery = {
+    query: '{node(id: "Gid2AorIFgJgIqqgIphAWXYkSJWF") { ... on Game {playsConnection(first: 1000) {nodes {description}}}}}'
+};
+
 aafApi.query(customGraphqlQuery, function(error, response, body) {
     if (error) {
         throw new Error(error);
@@ -24,7 +28,36 @@ aafApi.query(customGraphqlQuery, function(error, response, body) {
 
 For convenience, some GraphQL queries are already wrapped:
 
+### Get all scheduled season games
+
+```js
+aafApi.getSeasonScheduleGames(function(error, response, body) {
+    if (error) {
+        throw new Error(error);
+    } else {
+        console.log(body.data.seasonsConnection.nodes[0].gamesConnection.nodes);
+    }
+});
+```
+
 ### Get games (by date range)
+
+```js
+let dateRange = {
+    from: '2019-02-10T14:26:28.507Z',
+    to: '2019-02-11T02:26:28.507Z'
+};
+
+aafApi.getGamesByDateRange(dateRange, function(error, response, body) {
+    if (error) {
+        throw new Error(error);
+    } else {
+        console.log(body.data.gamesConnection.nodes);
+    }
+});
+```
+
+### Get live games (by date range)
 
 ```js
 let dateRange = {
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const request = require("request");
+const request = require('request');
 
 let aafApi = {};
 
 const postRequest = function(data, fn) {
-    var options = {
+    let options = {
         method: 'POST',
         url: 'https://api.platform.aaf.com/v1/graphql',
         json: true,
         headers: {
             'Cache-Control': 'no-cache',
             'Content-Type': 'application/json',
-            'User-Agent': 'node-aaf-api (0.0.1)'
+            'User-Agent': 'node-aaf-api (https://github.com/eyecatchup/node-aaf-api)'
         },
         body: data
     };
@@ -22,25 +22,81 @@ aafApi.query = function(requestPayload, fn) {
     postRequest(requestPayload, fn);
 };
 
-aafApi.getLiveGamesByDateRange = function(dateRange, fn) {
+aafApi.getSeasonScheduleGames = function(fn) {
     let requestPayload = {
-        operationName: 'getListOfLiveGameQuery',
-        variables: {
-            atOrAfterTime: dateRange.from,
-            beforeTime: dateRange.to
-        },
-        query: 'query getListOfLiveGameQuery($beforeTime: DateTime!, $atOrAfterTime: DateTime!) {\n' +
-               '  gamesConnection(beforeTime: $beforeTime, atOrAfterTime: $atOrAfterTime, first: 20) {\n' +
+        operationName: 'getSeasonScheduleGamesQuery',
+        variables: {},
+        query: 'query getSeasonScheduleGamesQuery {\n' +
+               '  seasonsConnection(last: 1) {\n' +
                '    nodes {\n' +
                '      id\n' +
-               '      clock {\n' +
-               '        seconds\n' +
-               '        time\n' +
+               '      gamesConnection(first: 60) {\n' +
+               '        nodes {\n' +
+               '          ...seasonGameFragment\n' +
+               '          __typename\n' +
+               '        }\n' +
+               '        __typename\n' +
+               '      }\n' +
+               '      __typename\n' +
+               '    }\n' +
+               '    __typename\n' +
+               '  }\n' +
+               '}\n' +
+               '\n' +
+               'fragment seasonGameFragment on Game {\n' +
+               '  id\n' +
+               '  timeToBeDetermined\n' +
+               '  subseason\n' +
+               '  time\n' +
+               '  awayTeam {\n' +
+               '    id\n' +
+               '    regionName\n' +
+               '    abbreviation\n' +
+               '    name\n' +
+               '    wordmark(style: LIGHT_BACKGROUND) {\n' +
+               '      id\n' +
+               '      url\n' +
+               '      __typename\n' +
+               '    }\n' +
+               '    __typename\n' +
+               '  }\n' +
+               '  homeTeam {\n' +
+               '    id\n' +
+               '    regionName\n' +
+               '    abbreviation\n' +
+               '    name\n' +
+               '    __typename\n' +
+               '  }\n' +
+               '  stadium {\n' +
+               '    id\n' +
+               '    name\n' +
+               '    __typename\n' +
+               '  }\n' +
+               '  __typename\n' +
+               '}\n'
+    };
+    postRequest(requestPayload, fn);
+};
+
+aafApi.getGamesByDateRange = function(dateRange, fn) {
+    let requestPayload = {
+        operationName: 'getListOfGameQuery',
+        variables: {
+            'atOrAfterTime': dateRange.from,
+            'beforeTime': dateRange.to,
+            'first': 20
+        },
+        query: 'query getListOfGameQuery($first: Int, $atOrAfterTime: DateTime!, $beforeTime: DateTime!) {\n' +
+               '  gamesConnection(first: $first, atOrAfterTime: $atOrAfterTime, beforeTime: $beforeTime) {\n' +
+               '    nodes {\n' +
+               '      id\n' +
+               '      time\n' +
+               '      status {\n' +
+               '        phase\n' +
                '        __typename\n' +
                '      }\n' +
                '      awayTeam {\n' +
                '        id\n' +
-               '        name\n' +
                '        abbreviation\n' +
                '        logo(style: LIGHT_BACKGROUND) {\n' +
                '          url\n' +
@@ -62,7 +118,6 @@ aafApi.getLiveGamesByDateRange = function(dateRange, fn) {
                '      }\n' +
                '      homeTeam {\n' +
                '        id\n' +
-               '        name\n' +
                '        abbreviation\n' +
                '        logo(style: LIGHT_BACKGROUND) {\n' +
                '          url\n' +
@@ -82,15 +137,60 @@ aafApi.getLiveGamesByDateRange = function(dateRange, fn) {
                '        }\n' +
                '        __typename\n' +
                '      }\n' +
+               '      ticketingWebsiteURL\n' +
+               '      availability {\n' +
+               '        shortName\n' +
+               '        url\n' +
+               '        __typename\n' +
+               '      }\n' +
+               '      stadium {\n' +
+               '        name\n' +
+               '        __typename\n' +
+               '      }\n' +
+               '      __typename\n' +
+               '    }\n' +
+               '    __typename\n' +
+               '  }\n' +
+               '}\n'
+    };
+    postRequest(requestPayload, fn);
+};
+
+aafApi.getLiveGamesByDateRange = function(dateRange, fn) {
+    let requestPayload = {
+        operationName: 'getListOfLiveGameQuery',
+        variables: {
+            atOrAfterTime: dateRange.from,
+            beforeTime: dateRange.to
+        },
+        query: 'query getListOfLiveGameQuery($beforeTime: DateTime!, $atOrAfterTime: DateTime!) {\n' +
+               '  gamesConnection(beforeTime: $beforeTime, atOrAfterTime: $atOrAfterTime, first: 50) {\n' +
+               '    nodes {\n' +
+               '      id\n' +
+               '      awayTeam {\n' +
+               '        id\n' +
+               '        name\n' +
+               '        abbreviation\n' +
+               '        logo(style: LIGHT_BACKGROUND) {\n' +
+               '          url\n' +
+               '          __typename\n' +
+               '        }\n' +
+               '        colors\n' +
+               '        __typename\n' +
+               '      }\n' +
+               '      homeTeam {\n' +
+               '        id\n' +
+               '        name\n' +
+               '        abbreviation\n' +
+               '        logo(style: LIGHT_BACKGROUND) {\n' +
+               '          url\n' +
+               '          __typename\n' +
+               '        }\n' +
+               '        colors\n' +
+               '        __typename\n' +
+               '      }\n' +
                '      status {\n' +
-               '        awayTeamPoints\n' +
-               '        homeTeamPoints\n' +
-               '        quarter\n' +
-               '        time\n' +
-               '        down\n' +
-               '        yardsToGo\n' +
                '        phase\n' +
-               '        possession\n' +
                '        __typename\n' +
                '      }\n' +
                '      stadium {\n' +
@@ -116,6 +216,11 @@ aafApi.getLiveGameData = function(gameId, fn) {
                '  node(id: $gameId) {\n' +
                '    ... on Game {\n' +
                '      id\n' +
+               '      clock {\n' +
+               '        seconds\n' +
+               '        time\n' +
+               '        __typename\n' +
+               '      }\n' +
                '      homeTeam {\n' +
                '        id\n' +
                '        name\n' +
